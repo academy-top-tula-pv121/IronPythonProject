a847f82ac6fa0d17d773cdb8efaf8c257d312e55
admin <admin@VTORAYA0>
2022-10-13 21:09:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace IronPythonProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 200;
+
+            ScriptEngine engine = Python.CreateEngine();
+            //engine.Execute("print('Hello NET from Iron Python!')");
+            ScriptScope scope = engine.CreateScope();
+
+            //scope.SetVariable("b", num);
+            //engine.ExecuteFile("../../../prog.py", scope);
+
+            //dynamic x = scope.GetVariable("a");
+            //dynamic y = scope.GetVariable("c");
+
+            //Console.WriteLine($"{x} + {num} = {y}");
+
+            int number = 1000;
+            engine.ExecuteFile("../../../prog.py", scope);
+            dynamic sum = scope.GetVariable("GaussSum");
+            Console.WriteLine(sum.GetType());
+
+            dynamic result = sum(number);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ prog.py
@@ -0,0 +1,17 @@
+﻿'''
+num = int(input("input number: "))
+if num % 2 == 0:
+	print("even")
+else:
+	print("odd")
+'''
+
+#a = 100
+#c = a + b
+#print("result = " + str(c))
+
+def GaussSum(n):
+	r = 0;
+	for i in range(1, n + 1):
+		r += i
+	return r
